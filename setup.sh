403ac20d1148718e26d1f0c0400486ea4ea8240f
rasmusoh <rasmusoh@users.noreply.github.com>
2020-02-09 17:08:40 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,4 @@ ln -s ~/dotfiles/.tmux.conf ~
 ln -s ~/dotfiles/.gitconfig ~
 ln -s ~/dotfiles/.eslint.json ~
 ln -s ~/dotfiles/.gitignore ~
-ln -s ~/dotfiles/.config/ ~/.config/
-ln -s ~/dotfiles/.Xresources ~
-chmod +x $HOME/.config/polybar/launch.sh
+ln -s ~/dotfiles/config.json/ ~
